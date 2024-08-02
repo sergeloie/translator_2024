01eb89796a15993d80d0115090652882cd0e1d7b
serge loie <serge.loie@yandex.ru>
2024-08-02 17:33:50 +0500

    start testing YandexService

--- src/main/java/ru/anseranser/translator_2024/Translator2024Application.java
+++ src/main/java/ru/anseranser/translator_2024/Translator2024Application.java
@@ -2,10 +2,12 @@ package ru.anseranser.translator_2024;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class Translator2024Application {
 
 	public static void main(String[] args) {
--- src/main/java/ru/anseranser/translator_2024/model/YandexRequestDTO.java
+++ src/main/java/ru/anseranser/translator_2024/model/YandexRequestDTO.java
@@ -1,19 +1,15 @@
 package ru.anseranser.translator_2024.model;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.util.List;
 
 @Setter
 @Getter
-@NoArgsConstructor
 public class YandexRequestDTO {
 
-    @Value("${FOLDER_ID_ENV}")
-    private String folderId;
+    private final String folderId = System.getenv("FOLDER_ID");
     private List<String> texts;
     private String targetLanguageCode;
 }
